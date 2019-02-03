48e98eb5b1868ef5f787ad481b4418a9a53df857
Patrick Dicker <mr.patrick.dicker@gmail.com>
2019-02-02 22:27:23 -0700

    Add new to list

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 $(function() {
-   const countElements = document.getElementsByTagName("UL")[0].childElementCount;
-
-
    
+$('form').submit(event => {
+event.preventDefault();
+    const newEntry = $('input').val();
+   
+    $('ul').append(`<li>
+    <span class="shopping-item">${newEntry}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`)
+
+});
+
+const countElements = document.getElementsByTagName("UL")[0].childElementCount;
+
    for (i = 1; i <= countElements; i++){
 };
 
